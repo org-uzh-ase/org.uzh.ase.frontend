7775fce02f22348b5115cbc6161442a69b1349ad
ltoedt <laura.toedtli@uzh.ch>
2020-04-25 19:50:51 +0200

    make ui a little nicer

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.sass']
 })
-export class QuestionComponent implements OnInit {
+export class QuestionComponent implements OnInit, OnChanges {
 
   @Input() description: string;
+  description1: string;
+  description2: string;
   
   constructor() { }
 
   ngOnInit() {
+    this.updateDescriptions() }
+
+  ngOnChanges(){
+    this.updateDescriptions()
+  }
+
+  updateDescriptions(){
+    var wordlist = this.description.split(" ");
+    if(this.description.length > 450){
+      var half = Math.floor(wordlist.length/2);
+      this.description1 = wordlist.slice(0, half).join(" ");
+      this.description2 = wordlist.slice(half, wordlist.length).join(" ");  
+    }else{
+      this.description1 = this.description;
+      this.description2 = "";
+    }
   }
 
 }
