e113319368d8dfa1495eb16501fc1e9a308e6788
ltoedt <laura.toedtli@uzh.ch>
2020-05-01 17:45:12 +0200

    bug fix proper reload game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent{
   title = 'The Very Stressful Movie Quiz';
+  welcome = true;
   startedGame = false;
   gameover = false;
   totalScore = 0;
 
   startGame(){
     this.startedGame = true
+    this.welcome = false
   }
 
   setGameOver(valueEmitted: integer){
     this.gameover = true;
     this.startedGame = false;
     this.totalScore = valueEmitted;
+  }
+
+  restartGame(){
+    this.startedGame = true;
+    this.gameover = false;
+  }
+
+  backToStart(){
+    this.welcome = true;
+    this.gameover = false;  
   }
 }
